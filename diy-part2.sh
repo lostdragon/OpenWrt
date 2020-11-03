2d0ca45a0ee0c4cff71ec45015f60e7273a3167c
lostdragon <dragon829@gmail.com>
2020-11-03 17:45:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,10 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+# add extra package
+git clone https://github.com/bin20088/luci-theme-butongwifi.git package/luci-theme-butongwifi
+git clone https://github.com/Repobor/luci-app-koolproxyR.git package/luci-app-koolproxyR
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
